4b2af383266551a97ba8d5c34c6b0f75c39ba5cd
Joshua Wong <joshuawong0412@hotmail.com>
2019-01-19 16:03:12 -0500

    On-Off for TPLINK,GET,POST,IFTTT

---
+++ Smart Home/View Controllers/SettingsVC.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsVC.swift
+//  Smart Home
+//
+//  Created by Joshua Wong on 2/1/2019.
+//  Copyright © 2019 Joshua Wong. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
